98fe6fcfd2f1b5a6501b016afaacd4296be85fb7
David Kim <dkhw90@gmail.com>
2022-12-23 10:53:45 +0100

    Add a way to close using keyboard press event

--- script.js
+++ script.js
@@ -6,26 +6,27 @@
 - How to close modal by clicking outside modal
 
 */
+const modalButtons = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+const modalOverlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
 
 const showModalEventHandler = () => {
-  const modal = document.querySelector('.modal');
-  const overlay = document.querySelector(".overlay");
   modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modalOverlay.classList.remove('hidden');
 }
 
 const closeModalEventHandler = () => {
-  const modal = document.querySelector('.modal');
-  const overlay = document.querySelector('.overlay');
   modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modalOverlay.classList.add('hidden');
 }
 
-const modalButtons = document.querySelectorAll('.show-modal');
 modalButtons.forEach(button => button.addEventListener('click', showModalEventHandler));
-
-const closeModal = document.querySelector('.close-modal');
 closeModal.addEventListener('click', closeModalEventHandler);
-
-const modalOverlay = document.querySelector('.overlay');
 modalOverlay.addEventListener('click', closeModalEventHandler);
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !modalOverlay.classList.contains('hidden')) {
+    closeModalEventHandler()
+  };
+})
